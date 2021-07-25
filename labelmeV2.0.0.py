f60172a501943758e7b2a4e4bc538849ba06d896
wxyice <920082206@qq.com>
2021-07-25 21:01:13 +0800

    labelme zh to En

--- labelmeV2.0.0.py
+++ labelmeV2.0.0.py
@@ -7,32 +7,30 @@ import datetime
 
 # ------------------------------------------------------------------
 # labelme V2.0
-# 功能：单像素修改功能，局部放大功能,剪裁图片,自动划分模式，涂画模式
-# 使用方法
-# 1. 配置路径，配置保存路径，修改图片
-# z：修改显示模式，覆盖模式，边缘勾勒模式
-# g：选择ROI区域，在changedesk窗口内进行单像素修改（会自动同步）
-# a：add模式，左键单击增加前景像素
-# e: era模式，左键单击减少前景像素
-# f：flip模式，左键单击翻转像素的前后景
-# s：保存修改后的label
-# q: 退出程序
-# c: 剪裁模式
-# o：自动划分模式
-#   ijkl分别控制对应框上下左右移动
-# b: 调整画框变大（每次放大1）
-# m：调整画框变小（每次缩小1，不可小于2倍）
-# pen：调整画笔大小
-# n：调整划分密度
+# Functions: 
+# single pixel modification function
+# local magnification function
+# clipping picture
+# automatic division mode
+# painting mode
 # 
-# 注：
-# 1. 覆盖模式下可以调整gamma调整覆盖层的透明度
-# 2. 按下左键拖动可以连续修改label
+# Instructions
+# 1. Configure path, configure save path, modify picture
 # 
-# 剪裁图片功能
-# 1. 按下c键，进入剪裁模式，设定cut width 表示保存下来的图片宽度大小（100-1200）
-# 2. 用鼠标框选需要的部分，然后回车
-# 3. 文件将被保存在cut文件下方
+# z：modify display mode, overlay mode and edge outline mode
+# g：select the ROI area (synchronized automatically)
+# a：add Mode, left click to increase foreground pixels
+# e: era Mode, left click to reduce foreground pixels
+# f：flip Mode, left click to flip the foreground and background of pixels
+# s：Save the modified label
+# q: quit
+# c: cut
+# o：automatic  mode
+#   ijkl  controls the corresponding box to move up, down, left and right respectively
+# b: adjust the frame to enlarge (1 at a time)
+# m：adjust the picture frame to become smaller (reduce it by 1 at a time, not less than 2 times)
+# pen：adjust pen size
+# n：adjust partition density
 #
 # 2021.6.28 wxyice
 # ------------------------------------------------------------------
@@ -65,12 +63,12 @@ def change_pixel(mode,x,y,x0,y0,img_label,pen=1):
         print('the x:y={0:3d}:{1:3d} change to {2:3d}'.format(x0+x//s,y0+y//s,0))
 
 
-def onmouse_for_change(event, x, y, flags, param):  # 创建回调函数
+def onmouse_for_change(event, x, y, flags, param):  
     global x0,y0,s,ix, iy,save
     global mode,pen
 
     # 单像素修改
-    if event == cv2.EVENT_LBUTTONDOWN:  # 按下左键
+    if event == cv2.EVENT_LBUTTONDOWN:  
         #print(x,y,'----------')
         change_pixel(mode,x,y,x0,y0,img_label,pen)
         ix,iy=x,y
@@ -100,7 +98,7 @@ def autocut(n,H,W):
 
 
 if __name__ == '__main__':
-    # 选择一张图像，配置input和true的路径路径,配置修改后label的保存路径
+    
     path_of_img=r'C:\Users\Pangzhentao\learn_keras\test data\final_test\true_label_by_hand\diameter_2.58mm velocity_118.2\cut_672\test_img\img5.jpg'
     path_of_label=r'C:\Users\Pangzhentao\learn_keras\test data\final_test\true_label_by_hand\diameter_2.58mm velocity_118.2\cut_672\ground_truth\gt5.jpg'
     path_of_pred=r'src_photo\enhance_data_jpg\train_label\00001.jpg'
@@ -115,19 +113,19 @@ if __name__ == '__main__':
     name_of_change='changedesk'
     name_of_scrope='scrope'
 
-    # 读取图像
-    img_src=cv2.imread(path_of_img)/255 # 0-1 方便叠加 3channal
-    img_label=cv2.imread(path_of_label,0) # 1channal
+    
+    img_src=cv2.imread(path_of_img)/255
+    img_label=cv2.imread(path_of_label,0) 
     if path_of_pred!=None:
         img_pred=cv2.imread(path_of_pred,0)
 
-    gamma=0.2 # 叠加阈值
+    gamma=0.2 
     mode_of_edge=False
 
-    cv2.namedWindow(name_of_window)                 # 创建空窗口
+    cv2.namedWindow(name_of_window)                 
     cv2.namedWindow(name_of_change)
 
-    cv2.setMouseCallback(name_of_change, onmouse_for_change)  # 将回调函数与窗口绑定
+    cv2.setMouseCallback(name_of_change, onmouse_for_change)  
 
     cv2.createTrackbar('gamma',name_of_window,0,100,print_gamma) 
     cv2.createTrackbar('cut_width',name_of_window,10,1200,print_gamma) 
@@ -135,14 +133,14 @@ if __name__ == '__main__':
 
     cv2.createTrackbar('n',name_of_change,4,8,print_gamma) 
 
-    # 初始化
+    # init
     ix,iy=-1,-1
     x0,y0=0,0
     s=1
     ROI=None
     initBB=None
     pen=1
-    mode='flip'  # 默认翻转模式
+    mode='flip'  
     save=False
     size_big=5
     padding=20
@@ -157,21 +155,21 @@ if __name__ == '__main__':
 
     while True:
         if mode_of_edge:
-            # 边缘显示模式
-            img=img_src.copy() # 保留原图 
+            # Edge display mode
+            img=img_src.copy() 
             img=img*255
             img = img.astype(np.uint8)
             edge_of_label=cv2.Canny(img_label,255,255)
-            img[edge_of_label>127]=[255,255,255] # 标记边缘为白色 
+            img[edge_of_label>127]=[255,255,255] 
         else:
-            #表示覆盖模式
+            # overlay mode
             img=img_src.copy()
             img_mask=img_src.copy()
             img_mask[img_label>127]=[0,0,1]
             img=(img*gamma+img_mask*(1-gamma))*255
             img = img.astype(np.uint8)
 
-        key=cv2.waitKey(1) & 0xFF   #获取键值
+        key=cv2.waitKey(1) & 0xFF   
         gamma = cv2.getTrackbarPos('gamma',name_of_window)
         cut_width=cv2.getTrackbarPos('cut_width',name_of_window)
  
@@ -185,17 +183,19 @@ if __name__ == '__main__':
 
         gamma = gamma/100
 
-        if key==ord('z'):# 按下z键，交换边缘显示模式和全局显示模式
+
+        # Press the Z key to switch between the edge display mode and the global display mode
+        if key==ord('z'):
             mode_of_edge= not mode_of_edge
-        elif key==ord('q'): # 按下q键，退出程序
+        elif key==ord('q'): # Press the Q key to exit the program
             break
-        elif key==ord('g'): # 按下g键，进入选择小框
+        elif key==ord('g'): # Press the G key to enter the selection box
             initBB = cv2.selectROI(name_of_window,img, fromCenter=False,showCrosshair=False) 
-        elif key==ord('a'): # 按下a键，进入add模式
+        elif key==ord('a'): # Press the a key to enter the add mode
             mode='add'
-        elif key==ord('e'): # 按下e键，进入擦除模式 
+        elif key==ord('e'): # Press the e key to enter the erase mode
             mode='era'
-        elif key==ord('f'): # 按下f键进入翻转模式
+        elif key==ord('f'): # Press the F key to enter the flip mode
             mode='flip'
         elif key==ord('s'):
             cv2.imwrite(save_label_path,img_label)
@@ -203,7 +203,7 @@ if __name__ == '__main__':
             
         elif key==ord('o'):
             auto= not auto
-        elif key==ord('u'):  # 按下u键 刷新
+        elif key==ord('u'):  # Press the U key to refresh
             img_src=cv2.imread(path_of_img)/255 
             cv2.imwrite(save_label_path,img_label)
             img_label=cv2.imread(save_label_path,0) 
@@ -213,7 +213,7 @@ if __name__ == '__main__':
                 s=2
         elif key==ord('b'):
             s+=1
-        elif key==ord('c'): # 进入cut 模式
+        elif key==ord('c'): # Enter cut mode
             mode='cut'
 
             img=img_src.copy()
@@ -249,10 +249,9 @@ if __name__ == '__main__':
             cv2.imwrite(os.path.join(save_cut_path,'{0}_inputcut.jpg'.format(time)),inputimg)
             cv2.imwrite(os.path.join(save_cut_path,'{0}_predcut.jpg'.format(time)),predimg)
             cv2.imwrite(os.path.join(save_cut_path,'{0}_adthcut.jpg'.format(time)),adthimg)
-            # 恢复flip模式
             mode='flip'
 
-        if auto: # 自动划分
+        if auto: 
             if key==ord('l'):
                 numbox+=1
                 numbox=min(numbox,n**2)
@@ -285,7 +284,7 @@ if __name__ == '__main__':
             #s=500//ROI.shape[0]
             ROI=cv2.resize(ROI,(ROI.shape[1]*s,ROI.shape[0]*s))
             cv2.imshow(name_of_change,ROI)
-        # 输出信息
+        # Output information
         info = [
             ('control','add=a,flip=f,era=e,quit=q,save=s,mode_of_edge=z,get ROI=g'),
             ("image name",path_of_img.split(r"/")[-1]),
@@ -301,13 +300,3 @@ if __name__ == '__main__':
         cv2.imshow(name_of_window,img)
 
     cv2.destroyAllWindows()
-
-
-
-    
-
-    
-    
-    
-  
-
